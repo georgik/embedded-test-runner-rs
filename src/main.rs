761ce309018f7af53ae5f87c658698bc22b1c86e
Juraj Michálek <juraj.michalek@espressif.com>
2023-12-05 10:34:54 +0100

    wip: refactoring just for runner

--- src/main.rs
+++ src/main.rs
@@ -1,284 +1,141 @@
 use clap::Parser;
-use std::path::PathBuf;
-use std::{fs, path::Path};
-use std::process::Stdio;
-use tokio::time::{self, Duration, timeout};
 use tokio::process::Command as TokioCommand;
+use std::process::Stdio;
+use std::path::PathBuf;
+use tokio::time::{timeout, Duration};
+use tokio::fs::File as TokioFile;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
-    project_path: String,
+    elf_path: String,
 
     #[arg(short, long)]
-    output_directory: String,
-
-    #[arg(short, long)]
-    continue_on_error: bool,
-
-    #[arg(short = 'n', long)]
-    skip_build: bool,
-
-    #[arg(short = 'j', long, default_value_t = 0)]
-    parallelism: usize,
-
-    #[arg(short, long, default_value = "wokwi")]
     service: String,
+
+    #[arg(short, long)]
+    timeout: Option<u64>,
+
+    #[arg(short = 'p', long)]
+    scenario: Option<PathBuf>,
+
+    #[arg(short, long)]
+    output_file: Option<PathBuf>,
 }
 
-#[derive(Clone)]
-struct TestCase {
-    file_path: String,
-    build_mode: String,
-}
+async fn run_test(args: Args) -> Result<bool, Box<dyn std::error::Error>> {
+    let elf_file_path = args.elf_path;
+    let selected_service = args.service;
+    let test_timeout_duration = args.timeout.map(Duration::from_secs);
+    let scenario_file_path = args.scenario;
+    let test_output_file_path = args.output_file;
 
-impl TestCase {
-    fn build(&self, project_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
-        let example_name = Path::new(&self.file_path)
-            .file_stem()
-            .and_then(std::ffi::OsStr::to_str)
-            .ok_or("Failed to extract example name")?;
+    let mut command_builder = match selected_service.as_str() {
+        "espflash" => {
+            let mut espflash_command = TokioCommand::new("espflash");
+            espflash_command.arg("flash")
+                .arg("-p")
+                .arg("/dev/tty.usbmodem1101")
+                .arg("--monitor")
+                .arg(&elf_file_path);
 
-        println!("Building {} in {} mode...", example_name, self.build_mode);
+                println!("Executing command: espflash flash --monitor {}", elf_file_path);
+            espflash_command
+        },
+        "qemu" => {
+            let mut qemu_command = TokioCommand::new("qemu-system-riscv32");
+            qemu_command.arg(&elf_file_path);
 
-        let mut command = std::process::Command::new("cargo");
-        command.args(["build", "--example", example_name])
-               .current_dir(project_path);
-        if self.build_mode == "release" {
-            command.arg("--release");
-        }
+            println!("Executing command: qemu-system-riscv32 {}", elf_file_path);
+            qemu_command
+        },
+        "wokwi" => {
+            let mut wokwi_command = TokioCommand::new("wokwi-cli");
+            wokwi_command.arg("--elf").arg(&elf_file_path);
 
-        let status = command.status()?;
-        if !status.success() {
-            return Err(Box::new(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                format!("Failed to build {}", example_name),
-            )));
-        }
+            let mut command_message = format!("Executing command: wokwi-cli --elf {}", elf_file_path);
 
-        Ok(())
-    }
-
-
-    async fn run(&self, project_path: &Path, output_directory: &Path, service: &str, continue_on_error: bool) -> Result<bool, Box<dyn std::error::Error>> {
-        let example_name = Path::new(&self.file_path)
-            .file_stem()
-            .and_then(std::ffi::OsStr::to_str)
-            .ok_or("Failed to construct ELF path")?;
-
-        let elf_path = format!(
-            "target/riscv32imac-unknown-none-elf/{}/examples/{}",
-            if self.build_mode == "debug" { "debug" } else { "release" },
-            example_name
-        );
-
-        let scenario_dir = Path::new("scenarios").join(&self.build_mode);
-        let scenario_file = scenario_dir.join(format!("{}.yaml", example_name));
-
-        let absolute_output_dir = output_directory.canonicalize()?;
-        let tmp_output_dir = absolute_output_dir.join("tmp");
-        fs::create_dir_all(&tmp_output_dir)?;
-        let serial_log_file = tmp_output_dir.join(format!("{}-{}.txt", example_name, self.build_mode));
-
-        let mut command_args = Vec::new();
-        let command_to_run = match service {
-            "espflash" => {
-                command_args.push("flash".to_string());
-                command_args.push("-p".to_string());
-                command_args.push("/dev/tty.usbmodem1101".to_string());
-                command_args.push("--monitor".to_string());
-                command_args.push(elf_path.clone());
-                "espflash"
-            },
-            "qemu" => {
-                if self.build_mode == "release" {
-                    command_args.push("--release".to_string());
-                }
-                "qemu-system-riscv32"
-            },
-            "wokwi" => {
-                command_args.push("--elf".to_string());
-                command_args.push(elf_path.clone());
-                command_args.push("--scenario".to_string());
-                command_args.push(scenario_file.to_str().ok_or("Failed to convert scenario path to string")?.to_string());
-                command_args.push("--timeout".to_string());
-                command_args.push("5000".to_string());
-                command_args.push("--serial-log-file".to_string());
-                command_args.push(serial_log_file.to_str().ok_or("Failed to convert path to string")?.to_string());
-                "wokwi-cli"
-            },
-            _ => {
-                return Err(Box::new(std::io::Error::new(
-                    std::io::ErrorKind::InvalidInput,
-                    "Unknown service",
-                )));
+            if let Some(scenario_path) = &scenario_file_path {
+                wokwi_command.arg("--scenario").arg(scenario_path);
+                command_message.push_str(&format!(" --scenario {}", scenario_path.display()));
             }
+
+            if let Some(timeout) = test_timeout_duration {
+                wokwi_command.arg("--timeout").arg(timeout.as_secs().to_string());
+                command_message.push_str(&format!(" --timeout {}", timeout.as_secs()));
+            }
+
+            if let Some(output_file_path) = &test_output_file_path {
+                wokwi_command.arg("--serial-log-file").arg(output_file_path);
+                command_message.push_str(&format!(" --serial-log-file {}", output_file_path.display()));
+            }
+
+            println!("{}", command_message);
+            wokwi_command
+        },
+        _ => return Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "Unknown service",
+        ))),
+    };
+
+    command_builder.stdout(Stdio::piped()).stderr(Stdio::piped());
+
+    let test_timeout_duration = args.timeout.map_or(Duration::from_secs(60), Duration::from_secs);
+
+    let mut child = command_builder.spawn()?;
+    let mut child_stdout = child.stdout.take().ok_or_else(|| std::io::Error::new(std::io::ErrorKind::Other, "Failed to take stdout"))?;
+    let mut child_stderr = child.stderr.take().ok_or_else(|| std::io::Error::new(std::io::ErrorKind::Other, "Failed to take stderr"))?;
+
+    let child_future = async {
+        let mut output_file = if selected_service == "espflash" {
+            if let Some(output_file_path) = &test_output_file_path {
+                Some(TokioFile::create(output_file_path).await?)
+            } else {
+                None
+            }
+        } else {
+            None
         };
 
-        let mut command = TokioCommand::new(command_to_run);
-        command.args(&command_args)
-               .current_dir(project_path)
-               .stdout(Stdio::piped())
-               .stderr(Stdio::piped());
+        let mut async_stdout = tokio::io::stdout();
+        let mut async_stderr = tokio::io::stderr();
 
-        let mut child = command.spawn()?;
-        let child_id = child.id().expect("Failed to get child process id");
+        let stdout_fut = tokio::io::copy(&mut child_stdout, &mut async_stdout);
+        let stderr_fut = tokio::io::copy(&mut child_stderr, &mut async_stderr);
 
-        let test_timeout = Duration::from_secs(5);
-        let test_passed = match timeout(test_timeout, child.wait_with_output()).await {
-            Ok(result) => match result {
-                Ok(output) => {
-                    if service == "espflash" {
-                        fs::write(&serial_log_file, &output.stdout)?;
-                    }
-                    output.status.success()
-                },
-                Err(e) => {
-                    eprintln!("Command execution error: {}", e);
-                    false
-                }
-            },
-            Err(_) => {
-                eprintln!("Test {} timed out", example_name);
-                kill_child_process(child_id).await?;
-                false
-            }
-        };
+        tokio::try_join!(stdout_fut, stderr_fut)?;
 
-        let result_dir = if test_passed { "passed" } else { "failed" };
-        let final_output_dir = absolute_output_dir.join(result_dir);
-        fs::create_dir_all(&final_output_dir)?;
-        fs::rename(&serial_log_file, final_output_dir.join(format!("{}-{}.txt", example_name, self.build_mode)))?;
-
-        if !test_passed && !continue_on_error {
-            return Err(Box::new(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "Test failed and continue_on_error is false",
-            )));
+        if let Some(mut file) = output_file {
+            child_stdout = child.stdout.take().ok_or_else(|| std::io::Error::new(std::io::ErrorKind::Other, "Failed to take stdout"))?;
+            child_stderr = child.stderr.take().ok_or_else(|| std::io::Error::new(std::io::ErrorKind::Other, "Failed to take stderr"))?;
+            tokio::io::copy(&mut child_stdout, &mut file).await?;
+            tokio::io::copy(&mut child_stderr, &mut file).await?;
         }
 
-        Ok(test_passed)
-    }
-}
+        child.wait_with_output().await
+    };
 
-
-fn discover_test_cases(path: &Path) -> Vec<TestCase> {
-    let mut test_cases = Vec::new();
-
-    let examples_path = path.join("examples");
-    if examples_path.is_dir() {
-        if let Ok(entries) = fs::read_dir(examples_path) {
-            for entry in entries.flatten() {
-                let path = entry.path();
-                if path.is_file() && path.extension().and_then(std::ffi::OsStr::to_str) == Some("rs") {
-                    test_cases.push(TestCase {
-                        file_path: path.to_string_lossy().into_owned(),
-                        build_mode: "debug".to_string(),
-                    });
-                    test_cases.push(TestCase {
-                        file_path: path.to_string_lossy().into_owned(),
-                        build_mode: "release".to_string(),
-                    });
-                }
-            }
-        }
-    }
-    test_cases.sort_by(|a, b| a.file_path.cmp(&b.file_path));
-    test_cases
-}
-
-async fn kill_child_process(child_id: u32) -> Result<(), Box<dyn std::error::Error>> {
-    // Use a command line utility to kill the process
-    let kill_command = std::process::Command::new("kill")
-        .arg(format!("{}", child_id))
-        .spawn()?
-        .wait()
-        .expect("Failed to kill the child process");
-
-    if kill_command.success() {
-        Ok(())
-    } else {
-        Err(Box::new(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "Failed to kill the timed out process",
-        )))
+    match timeout(test_timeout_duration, child_future).await {
+        Ok(output_result) => output_result.map(|output| output.status.success()).map_err(Into::into),
+        Err(_) => {
+            eprintln!("Test execution timed out after {:?} seconds", test_timeout_duration.as_secs());
+            Ok(false)
+        },
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
+    let test_passed = run_test(args).await?;
 
-    let project_path = PathBuf::from(&args.project_path);
-    let output_directory = PathBuf::from(&args.output_directory);
-
-    let service = args.service;
-    let parallelism = if args.parallelism == 0 { num_cpus::get() } else { args.parallelism };
-
-    let test_cases = discover_test_cases(&project_path);
-
-    let mut passed_tests = 0;
-    let mut failed_tests = 0;
-    let mut total_build_time = Duration::new(0, 0);
-    let mut total_test_time = Duration::new(0, 0);
-
-    if !args.skip_build {
-        for test in &test_cases {
-            let start = time::Instant::now();
-            test.build(&project_path)?;
-            total_build_time += start.elapsed();
-        }
+    if test_passed {
+        println!("Test passed");
+    } else {
+        println!("Test failed");
     }
-
-    let test_start = time::Instant::now();
-
-    let mut handles = Vec::new();
-    for test in test_cases {
-        let project_path_clone = project_path.clone();
-        let output_directory_clone = output_directory.clone();
-        let service_clone = service.clone();
-
-        let handle = tokio::spawn(async move {
-            test.run(&project_path_clone, &output_directory_clone, &service_clone, args.continue_on_error).await
-                .map(|result| (result, test.file_path.clone(), test.build_mode.clone()))
-                .unwrap_or_else(|e| {
-                    println!("Error: {}", e);
-                    (false, test.file_path.clone(), test.build_mode.clone())
-                })
-        });
-
-        handles.push(handle);
-        if handles.len() == parallelism {
-            for handle in handles.drain(..) {
-                let (result, file_path, build_mode) = handle.await.unwrap();
-                if result {
-                    passed_tests += 1;
-                } else {
-                    failed_tests += 1;
-                }
-                println!("Test {} in {} mode: {}", file_path, build_mode, if result { "passed" } else { "failed" });
-            }
-        }
-    }
-
-    for handle in handles {
-        let (result, file_path, build_mode) = handle.await.unwrap();
-        if result {
-            passed_tests += 1;
-        } else {
-            failed_tests += 1;
-        }
-        println!("Test {} in {} mode: {}", file_path, build_mode, if result { "passed" } else { "failed" });
-    }
-
-    total_test_time += test_start.elapsed();
-
-    println!("Test run summary:");
-    println!("Total build time: {:?}", total_build_time);
-    println!("Total test time: {:?}", total_test_time);
-    println!("Passed tests: {}", passed_tests);
-    println!("Failed tests: {}", failed_tests);
 
     Ok(())
 }
